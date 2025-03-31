cb2a4c7cd7707609a18201d2d862417a88f61ba6
lyaronskaya <luba.yaronskaya@gmail.com>
2025-03-31 22:00:57 +0200

    fix

--- react/src/index.ts
+++ react/src/index.ts
@@ -1,11 +1,13 @@
+export { TextNeuronActivations } from "./activations/TextNeuronActivations";
 export { AttentionHeads } from "./attention/AttentionHeads";
 export { AttentionPattern } from "./attention/AttentionPattern";
 export { AttentionPatterns } from "./attention/AttentionPatterns";
+export { Hello } from "./examples/Hello";
+export { InformationFlowGraph } from "./InformationFlow/InformationFlowGraph";
+export { TokenLogProbs } from "./logits/TokenLogProbs";
+export { render } from "./render-helper";
 export { ColoredTokens } from "./tokens/ColoredTokens";
 export { ColoredTokensMulti } from "./tokens/ColoredTokensMulti";
-export { Hello } from "./examples/Hello";
-export { render } from "./render-helper";
-export { TextNeuronActivations } from "./activations/TextNeuronActivations";
-export { TokenLogProbs } from "./logits/TokenLogProbs";
 export { TopkSamples } from "./topk/TopkSamples";
 export { TopkTokens } from "./topk/TopkTokens";
+
